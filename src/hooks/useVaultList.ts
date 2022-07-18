dfb39d24ecdfa55102f2a3d655b98a96b6cd52e0
Greypixel <greypixel@gpxl.tech>
2022-07-18 20:23:09 +0100

    Change vaultList to use react-query

--- src/hooks/useVaultList.ts
+++ src/hooks/useVaultList.ts
@@ -1,17 +1,28 @@
+import { useQuery } from 'react-query'
 import { VaultDetails } from '../types'
 
-export function useVaultList(): VaultDetails[] {
-  return [
-    {
-      name: 'Umami WETH-WBTC-USDC',
-      rToken: 'umWETH-WBTC-USDC',
-      aToken: '0x64541216bAFFFEec8ea535BB71Fbc927831d0595',
-      type: 'Autocompounder',
-      address: '0x5F945110043605a93CFc27A5699EA81f6c867aeB',
-      details:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      fees: '0.2-1.5-15-0',
-      zapper: '',
-    },
-  ]
+export function useVaultList() {
+  const fetchVaults: () => null | Promise<VaultDetails[]> = () => {
+    // TODO: this is just a mock for now - should request from server.
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve([
+          {
+            name: 'umWETH-WBTC-USDC',
+            rToken: 'umWETH-WBTC-USDC',
+            aToken: '0x64541216bAFFFEec8ea535BB71Fbc927831d0595',
+            type: 'Autocompounder',
+            address: '0x5F945110043605a93CFc27A5699EA81f6c867aeB',
+            details:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            fees: '0.2-1.5-15-0',
+            zapper: '',
+          },
+        ])
+      }, 2500)
+    })
+  }
+  return useQuery<VaultDetails[] | null, Error>(['vaults'], fetchVaults, {
+    staleTime: 10 * 60 * 1000,
+  })
 }
--- src/pages/Vaults.tsx
+++ src/pages/Vaults.tsx
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet'
 import PageContent from '../components/PageContent'
 import VaultCard from '../components/VaultCard'
 import { useGlpTcrUsdcPoolInfo } from '../hooks/useGlpTcrUsdcPoolInfo'
+import { useVaultList } from '../hooks/useVaultList'
 
 export default function Vaults() {
   const { data: poolInfo } = useGlpTcrUsdcPoolInfo()
